3de5c5cc8f75cc52ab002c6cee9cee03b70918ab
liu24567890 <1627731951@qq.com>
2021-06-25 02:48:18 -0400

    hello earth

--- hello.c
+++ hello.c
@@ -6,6 +6,8 @@ int main(int argc, const char *argv[])
 	printf("hello world\n");
 	printf("hello LHB\n");
 	printf("hello erath\n");
+	printf("hello LHB\n");
+	printf("hello LHB\n");
 
 	return 0;
 }
